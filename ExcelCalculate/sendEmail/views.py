a06a7f4ed33bd6530b967a3ed8e047dff8b87270
walter <twer4774@gmail.com>
2020-08-20 14:45:23 +0900

    create: Project Setting

--- ExcelCalculate/sendEmail/views.py
+++ ExcelCalculate/sendEmail/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def send(receiverEmail, verifyCode):
+    return HttpResponse("sendEamil, send funciton")
